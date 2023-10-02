a3becd4a01e6947494fa67436a696784f24e6583
Dylan <dylanlarkin66@gmail.com>
2023-10-02 00:49:06 -0600

    restored database

--- website/__init__.py
+++ website/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     # App configuration
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['STRIPE_PUBLIC_KEY'] = "pk_live_51N6HKxLW7Q4gXOtz6rxys8vMr1VSjaIE8dbVrroot5HFOFyZQJVpnlL5hZidkoBDo4WHhcNP8eBx63VNdSFoylke00WbbE7t1x"
     app.config['STRIPE_SECRET_KEY'] =  os.environ.get('STRIPE_SECRET_KEY')
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
